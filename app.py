c88a5b4702f44b19fa8dc581996666d3da8ff12c
Sung Kim <hunkim@gmail.com>
2017-07-23 23:06:05 +0900

    Added K

--- app.py
+++ app.py
@@ -47,17 +47,9 @@ line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-@app.route('/message')
+@app.route('/message', methods=['POST'])
 def message():
-    request = app.current_request
-    body = request.raw_body.decode('utf-8')
-    print('body: ', body)
-
-    return {"message": {"text": body}}
+    return {"message": {"text": 'Hello'}}
 
 @app.route('/keyboard')
 def keyboard():
